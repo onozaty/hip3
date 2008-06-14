165793bf5eb16906f59c8660b9688d9151408fbf
onozaty <onozaty@gmail.com>
2008-06-14 16:00:52 +0000

    added build set, and archive file.

---
+++ hip/startup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# ---------------------------------------------------
+# HIP Startup.
+# ---------------------------------------------------
+
+HIP_CLASSPATH='hip-0.1.jar:./lib/jetty-6.1.9.jar:./lib/jetty-util-6.1.9.jar:./lib/logbot.jar:./lib/pircbot.jar:./lib/servlet-api-2.5-6.1.9.jar'
+
+java -classpath %HIP_CLASSPATH% com.enjoyxstudy.hip.HipServer

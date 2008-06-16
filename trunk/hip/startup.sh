c8effc33a44029aae757de8931e3f2f3cf8c7185
onozaty <onozaty@d69323c3-784f-0410-8b25-87718e594ec8>
2008-06-16 13:55:56 +0000

    fixed bug(classpath setting).

--- trunk/hip/startup.sh
+++ trunk/hip/startup.sh
@@ -5,4 +5,4 @@
 
 HIP_CLASSPATH='hip-0.1.jar:./lib/jetty-6.1.9.jar:./lib/jetty-util-6.1.9.jar:./lib/logbot.jar:./lib/pircbot.jar:./lib/servlet-api-2.5-6.1.9.jar'
 
-java -classpath %HIP_CLASSPATH% com.enjoyxstudy.hip.HipServer
+java -classpath $HIP_CLASSPATH com.enjoyxstudy.hip.HipServer
